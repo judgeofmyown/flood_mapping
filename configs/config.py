b47167b047f8d73b3487b7526c1e5439bf04e21c
judgeofmyown <tanbir100000@gmail.com>
2026-07-26 01:03:51 +0530

    last updates

--- configs/config.py
+++ configs/config.py
@@ -1,24 +1,31 @@
-OUT_DIR = "/kaggle/working/"
+import os
+from pathlib import Path
 
-EPOCHS = 25
-LR = 4.0e-5
-WEIGHT_DECAY = 0.1
-HEAD_DROPOUT = 0.3
-FREEZE_BACKBONE = False
-CLASS_WEIGHTS = [1, 1.5]
+def get_root_dir() -> Path:
+    return Path(__file__).parent.parent
 
-BANDS = ['HH', 'HV', 'Green', 'Red', 'NIR', 'SWIR']
-NUM_FRAMES=1
-NUM_WORKERS=4
+BASE_DIR = get_root_dir()
+os.mkdir("\output")
+OUT_DIR = os.path.join(BASE_DIR, "\output")
+EPOCHS              = 25
+LR                  = 4.0e-5
+WEIGHT_DECAY        = 0.1
+HEAD_DROPOUT        = 0.3
+FREEZE_BACKBONE     = False
+CLASS_WEIGHTS       = [1, 1.5]
 
-BACKBONE_NAME =  "prithvi_eo_v2_tiny_tl"
+BANDS               = ['HH', 'HV', 'Green', 'Red', 'NIR', 'SWIR']
+NUM_FRAMES          = 1
+NUM_WORKERS         = 4
 
-IMG_PATH = ""
-PRED_IMG_PATH = ""
-LABEL_PATH = ""
-SPLIT_PATH_TRAIN = ""
-SPLIT_PATH_TEST = ""
-SPLIT_PATH_VAL = ""
-SPLIT_PATH_PRED = ""
+BACKBONE_NAME       = "prithvi_eo_v2_tiny_tl"
 
-SEED = 0
+IMG_PATH            = os.path.join(BASE_DIR, "data\image")
+PRED_IMG_PATH       = os.path.join(BASE_DIR, "data\prediction\image")
+LABEL_PATH          = os.path.join(BASE_DIR, "data\label")
+SPLIT_PATH_TRAIN    = os.path.join(BASE_DIR, "data\split\train.txt")
+SPLIT_PATH_TEST     = os.path.join(BASE_DIR, "data\image\test.txt")
+SPLIT_PATH_VAL      = os.path.join(BASE_DIR, "data\image\val.txt")
+SPLIT_PATH_PRED     = os.path.join(BASE_DIR, "data\image\pred.txt")
+
+SEED                = 0
